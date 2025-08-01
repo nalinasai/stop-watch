c6d651c96dc87a29469099649cc87d44b3767035
nalinasai <nalinanalina995@gmail.com>
2025-08-01 14:17:39 +0530

    fix the minute update

--- script.js
+++ script.js
@@ -1,86 +1,43 @@
-var startbutton = document.getElementById("start")
-var stopbutton = document.getElementById("stop")
-var resetbutton = document.getElementById("reset")
-var h = document.getElementById("hour")
-var m = document.getElementById("minute")
-var s = document.getElementById("second")
+  var startbutton = document.getElementById("start");
+  var stopbutton = document.getElementById("stop");
+  var resetbutton = document.getElementById("reset");
 
+  var h = document.getElementById("hour");
+  var m = document.getElementById("minute");
+  var s = document.getElementById("second");
 
-let timer = null
-function updatesecond(){
-    
-    
-        let svalue = Number(s.innerText) + 1
-        if(svalue<10){
-            s.innerText = "0" + svalue
-        }
-        else{
-            s.innerText = svalue
-        }
-        
-        if(svalue==60){
-            s.innerText = "00"
-            
-            function updateminute(){
-                let mvalue = Number(m.innerText) + 1
-                if(mvalue<10){
-                    m.innerText = "0" + mvalue
-                }
-                else{
-                    m.innerText = mvalue
-                }
+  let timer = null;
+  let totalSeconds = 0;
 
-                if(mvalue==60){
-                    m.innerText="00"
-                    
-                    function updatehour(){
-                        let hvalue = Number(h.innerText) + 1
-                        if(hvalue<10){
-                            h.innerText = "0" + hvalue
-                        }
-                        else{
-                            h.innerText = hvalue
-                        }
-                        
-                    }
-                    if(!timer){
-                        updatehour()
-                    }
-                }
-            }
-            if(!timer){
-                updateminute()
-            }
+  function updateDisplay() {
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor((totalSeconds % 3600) / 60);
+    let seconds = totalSeconds % 60;
 
+    h.innerText = hours < 10 ? "0" + hours : hours;
+    m.innerText = minutes < 10 ? "0" + minutes : minutes;
+    s.innerText = seconds < 10 ? "0" + seconds : seconds;
+  }
 
+  function tick() {
+    totalSeconds++;
+    updateDisplay();
+    timer = setTimeout(tick, 1000);
+  }
 
-        }
-        
-        timer = setTimeout(()=>{
-            updatesecond()
-        },1000)
+  function start() {
+    if (!timer) {
+      tick();
     }
+  }
 
+  function stop() {
+    clearTimeout(timer);
+    timer = null;
+  }
 
-function start(button){
-    if(!timer){
-        
-        updatesecond()
-    }
-    
-    
-}
-
-function stop(){
-    clearTimeout(timer)
-    timer = null
-}
-
-function reset(){
-    stop()
-    h.innerText = "00"
-    m.innerText = "00"
-    s.innerText = "00"
-    
-    
-}
+  function reset() {
+    stop();
+    totalSeconds = 0;
+    updateDisplay();
+  }
